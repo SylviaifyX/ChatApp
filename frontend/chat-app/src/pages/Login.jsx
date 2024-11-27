2a565fde744e4459997187d3494b6e794525ba59
SylviaifyX <ifeomasylviaokpara@gmail.com>
2024-11-27 13:28:32 +0100

    login/signup linked with the backend

--- frontend/chat-app/src/pages/Login.jsx
+++ frontend/chat-app/src/pages/Login.jsx
@@ -1,51 +1,83 @@
 
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react"
+import axios from "axios"
 
-const Login = () =>{
-    return(
-        <div className="w-full h-screen bg-green-50 flex flex-col items-center justify-center p-3">
-        <div className="w-full max-w-[500px] flex flex-col mx-auto  gap-3 font-font text-[18px]">
-          <h1 className="text-[30px]">
-            <span className="text-[48px]">F</span>army
-          </h1>
-          <form className="form flex flex-col gap-5">
-            <input
-              type="text"
-              name=""
-              id=""
-              placeholder="email"
-              className="p-3 outline-none rounded border border-green-600"
-              required
-            />
-            <input
-              type="text"
-              name=""
-              id=""
-              placeholder="Password"
-              className="p-3 outline-none rounded border border-green-600"
-              required
-            /> 
-            <button
-              type="submit"
-              className="bg-green-800 p-3 rounded outline-none
+const Login = () => {
+  const [personName, setPersonName] = useState('')
+  const [personPassword, setPersonPassword] = useState('')
+  const [error, setError] = useState('');
+  const navigate = useNavigate()
+
+  const handleSummit = async (e) => {
+    e.preventDefault()
+    const dataDetails = {
+      username: personName,
+      password: personPassword
+    }
+
+    try {
+      const { data } = await axios.post("http://localhost:5005/auth/login", dataDetails)
+   
+      console.log(data)
+      if (data.token) {
+        localStorage.setItem("token", data.token);
+        navigate("/home");
+      } else {
+        navigate("/signup")
+        alert("You are not registered to this service")
+      }
+
+    } catch (error) {
+      console.log("there is an an loging in", error)
+    }
+  }
+  return (
+    <div className="w-full h-screen bg-green-50 flex flex-col items-center justify-center p-3">
+      <div className="w-full max-w-[500px] flex flex-col mx-auto  gap-3 font-font text-[18px]">
+        <h1 className="text-[30px]">
+          <span className="text-[48px]">F</span>army
+        </h1>
+        <form className="form flex flex-col gap-5" onSubmit={handleSummit}>
+          <input
+            type="text"
+            name="username"
+            value={personName}
+            placeholder="name"
+            className="p-3 outline-none rounded border border-green-600"
+            required
+            onChange={(e) => setPersonName(e.target.value)}
+          />
+          <input
+            type="password"
+            name="password"
+            value={personPassword}
+            placeholder="Password"
+            className="p-3 outline-none rounded border border-green-600"
+            required
+            onChange={(e) => setPersonPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-green-800 p-3 rounded outline-none
                        text-white text-[18px]
                       hover:bg-white
                        hover:text-black hover:border
                        hover:border-green-700"
-            >
-              Login
-            </button>
-          </form>
-  
-          <div className="flex items-center gap-2">
-            <p className="text-[16px]">Don't have an account?</p>{" "}
-            <span className="text-green-800 font-medium">
-              <Link to="/signup">SignUp</Link>
-            </span>
-          </div>
+          >
+            Login
+          </button>
+        </form>
+
+        <div className="flex items-center gap-2">
+          <p className="text-[16px]">Don't have an account?</p>{" "}
+          <span className="text-green-800 font-medium">
+            <Link to="/signup">SignUp</Link>
+          </span>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Login
--- frontend/chat-app/src/pages/SignUp.jsx
+++ frontend/chat-app/src/pages/SignUp.jsx
@@ -1,36 +1,60 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useState } from "react"
+import axios from "axios"
+import io from "socket.io-client"
+
+const socket = io("http://localhost:5005")
+
+
 const SignUp = () => {
+  const [personName, setPersonName] = useState('')
+  const [personPassword, setPersonPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [successMessage, setSuccessMessage] = useState('')
+  const navigate = useNavigate()
+
+  const handleSignUp = async (e) => {
+    e.preventDefault()
+    // console.log("sign email", email , "password", personPassword, "personname", personName)
+    try {
+      const response = await axios.post("http://localhost:5005/auth/register", 
+        { username: personName,  password: personPassword })
+      setSuccessMessage("resgistration successful")
+      console.log("Response", response)
+      setPersonName("");
+      setPersonPassword("");
+
+      navigate("/login")
+    } catch (error) {
+      setErrorMessage(
+        error.response?.data?.message || "An error occurred during registration."
+      );
+    }
+  };
+
   return (
     <div className="w-full h-screen bg-green-50 flex flex-col items-center justify-center p-3">
       <div className="w-full max-w-[500px] flex flex-col mx-auto  gap-3 font-font text-[18px]">
         <h1 className="text-[30px]">
           <span className="text-[48px]">F</span>army
         </h1>
-        <form className="form flex flex-col gap-5">
-        <input
-            type="text"
-            name=""
-            id=""
+        <form className="form flex flex-col gap-5" onSubmit={handleSignUp}>
+          <input
+            type="name"
+            value={personName}
+            onChange={(e) => setPersonName(e.target.value)}
             placeholder="Name"
             className="p-3 outline-none rounded border border-green-600"
             required
           />
           <input
-            type="text"
-            name=""
-            id=""
-            placeholder="email"
-            className="p-3 outline-none rounded border border-green-600"
-            required
-          />
-          <input
-            type="text"
-            name=""
-            id=""
+            type="password"
+            value={personPassword}
+            onChange={(e) => setPersonPassword(e.target.value)}
             placeholder="Password"
             className="p-3 outline-none rounded border border-green-600"
             required
-          /> 
+          />
           <button
             type="submit"
             className="bg-green-800 p-3 rounded outline-none
@@ -42,6 +66,12 @@ const SignUp = () => {
             Register
           </button>
         </form>
+        {successMessage && (
+          <p className="text-green-700 text-center">{successMessage}</p>
+        )}
+        {errorMessage && (
+          <p className="text-red-600 text-center">{errorMessage}</p>
+        )}
 
         <div className="flex items-center gap-2">
           <p className="text-[16px]">Already have an account?</p>{" "}
